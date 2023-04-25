52eac0a551b1aada1a609895db66f74ae09890b4
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-25 09:25:10 -0600

    Feature: add all methods.

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,26 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain
+
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if @loop == false
+      total_distance = @distance * 2
+    else
+      total_distance = @distance
+    end
+    total_distance
+  end
 
 end
